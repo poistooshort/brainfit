4a4f166fc716835f195094d89cbdc5a83bd5c5fe
Po Lin <po.ting.lin@gmail.com>
2022-08-06 11:47:59 -0400

    Add basic jsx structure to Upload.js. Begin handleUpload

--- src/pages/Upload/Upload.js
+++ src/pages/Upload/Upload.js
@@ -1,10 +1,54 @@
+import { useState } from 'react';
+import axios from 'axios';
+
 import './Upload.scss';
 
-const Upload = () => {
+const Upload = ({ user }) => {
+	const [file, setFile] = useState('');
+	const [progress, setProgress] = useState(0);
+
+	const handleUpload = (e) => {
+		e.preventDefault();
+
+		const { title, description, image } = e.target;
+		console.log("@handleUpload"); //TEST
+		console.log(e); //TEST
+		console.log('title :', title.value);
+		console.log('description :', description.value);
+	}
+
 	return(
 		<div className="upload">
-			<form>
-			</form>
+			<form className="upload__form" onSubmit={handleUpload}>
+
+				<label htmlFor="image">exercise gif: 
+					<input 
+						className="upload__choose-file"
+						id="image"
+						name="image"
+						type="file">
+					</input>
+				</label>
+
+				<label htmlFor="title"> exercise title:
+					<input 
+						id="title" 
+						name="title" 
+						type="text" 
+						placeholder="exercise title">
+					</input>
+				</label>
+
+				<label htmlFor="description"> exercise description:
+					<textarea 
+						id="description" 
+						name="description" 
+						placeholder="exercise description">
+					</textarea>
+				</label>
+
+				<button type="submit">upload</button>
+			</form>			
 		</div>
 	);
 }
