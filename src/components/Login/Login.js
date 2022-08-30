c2ab4215cf4d1d1e8a038bfd7672241a7abbfc42
Po Lin <po.ting.lin@gmail.com>
2022-08-29 21:35:44 -0400

    Start adding skeleton out a way to login with jwt using username password sign in

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -8,6 +8,14 @@ const Login = () => {
 		<div className="login">
 			<div className="login__card">
 				<h1 className="login__title">login page</h1>
+				<div className="login__sign-in-container">
+					<form className="login__sign-in">
+						<label className="login__username-input" htmlFor=""></label>
+						<input className="login__username-input" name="username" id="username" type="text">
+						<label className="login__password-label" htmlFor=""></label>
+						<input className="login__password-input" name="password" id="password" type="password">
+					</htmlForm>
+				</div>
 				<p className="login__text">please log in to view profile page</p>
 				<a className="login__github" href={`${SERVER_URL}/auth/github`}>
 					<img src={githubIcon} alt="github logo"/>
--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -2,10 +2,11 @@ import './Homepage.scss';
 import exercise from '../../assets/images/exercise.gif';
 
 const Homepage = () => {
+
 	return(
 		<div className="homepage">
 			<div className="homepage__title-greeting-container">
-				<h1 className="homepage__title">brainFit</h1>	
+				<h1 className="homepage__title">brainFit</h1>
 				<p className="homepage__greeting">{`"Exercise not only changes your body, it changes your mind"`}</p>
 			</div>
 			<img src={exercise} className="homepage__image" alt="animated exercising person"/>
