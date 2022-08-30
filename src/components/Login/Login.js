65b64ac562128cad313ad769f1e099da6a0c4388
Po Lin <po.ting.lin@gmail.com>
2022-08-29 21:38:13 -0400

    Fix small semantic errors

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -11,10 +11,10 @@ const Login = () => {
 				<div className="login__sign-in-container">
 					<form className="login__sign-in">
 						<label className="login__username-input" htmlFor=""></label>
-						<input className="login__username-input" name="username" id="username" type="text">
+						<input className="login__username-input" name="username" id="username" type="text" />
 						<label className="login__password-label" htmlFor=""></label>
-						<input className="login__password-input" name="password" id="password" type="password">
-					</htmlForm>
+						<input className="login__password-input" name="password" id="password" type="password" />
+					</form>
 				</div>
 				<p className="login__text">please log in to view profile page</p>
 				<a className="login__github" href={`${SERVER_URL}/auth/github`}>
