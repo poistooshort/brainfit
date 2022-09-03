084129756ad9bcba55dbc111fc7614db5f89d360
Po Lin <po.ting.lin@gmail.com>
2022-09-03 00:53:55 -0400

    Change autocomplete to autoComplete on input tags of login and signup

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -8,17 +8,23 @@ const Signup = (props) => {
 		//return the user back to the login page
 	}
 
+	const handleSignup = (e) => {
+		e.preventDefault();
+
+		//sign up process
+	}
+
 	return(
 		<div className="signup">
 			<div className="signup__card">
 				<h1 className="signup__title">sign up page</h1>
 				<form className="signup__entry-form" action="">
 					<label className="signup__label" htmlFor="username">username:</label> 
-					<input className="signup__input" name="username" id="username" placeholder="choose username" autocomplete="username" type="text"/>
+					<input className="signup__input" name="username" id="username" placeholder="choose username" autoComplete="username" type="text"/>
 					<label className="signup__label" htmlFor="password">password:</label>
-					<input className="signup__input" name="password" id="password" placeholder="choose password" autocomplete="new-password" type="password"/>
+					<input className="signup__input" name="password" id="password" placeholder="choose password" autoComplete="new-password" type="password"/>
 					<label className="signup__label" htmlFor="passwordRepeat">repeat password:</label>
-					<input className="signup__input" name="passwordRepeat" id="passwordRepeat" placeholder="repeat chosen password" auto-complete="new-password" type="password"/>
+					<input className="signup__input" name="passwordRepeat" id="passwordRepeat" placeholder="repeat chosen password" autoComplete="new-password" type="password"/>
 					<label className="signup__avatar-upload" htmlFor="avatar">upload avatar img:</label>
 					<input className="signup__input-avatar" name="avatar" id="avatar" type="file"/>
 					<div className="signup__button-row">
